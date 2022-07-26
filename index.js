b2e3caa115ed9f1fb1f9f8efaf1d79c6436028b6
sajolbhadra <sajolbhadra@gmail.com>
2022-07-27 01:36:47 +0600

    error

--- index.js
+++ index.js
@@ -90,6 +90,6 @@ app.get('/', (req, res) => {
     res.send('Server Online');
 })
 
-app.listen(port, () => {
-    console.log('Server on port', port)
+app.listen(PORT, () => {
+    console.log('Server on port', PORT)
 })
